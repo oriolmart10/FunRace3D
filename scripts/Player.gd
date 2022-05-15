524bd935fc783e1bfd9ca8b0476d473827524933
Oriol Martín <oriol.martin.olive@estudiantat.upc.edu>
2022-05-15 16:55:52 +0200

    Animacions jugador arreglades

---
+++ scripts/Player.gd
@@ -0,0 +1,20 @@
+extends PathFollow
+
+export(float) var velocity = 0.0
+var current_state = RUN
+
+onready var animationPlayer = $Player/AnimationPlayer
+
+enum {RUN, IDLE}
+
+func _process(delta):
+	if Input.is_action_pressed("ui_walk"):
+		current_state = RUN
+		offset = offset + velocity
+		if current_state == RUN:
+			animationPlayer.play("Run")
+	else:
+		current_state = IDLE
+		animationPlayer.play("Idle")
+	
+	
