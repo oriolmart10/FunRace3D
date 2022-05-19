027a5ee5fb2fcc8891ad08355e0401dfba7eccda
Oriol Martín <oriol.martin.olive@estudiantat.upc.edu>
2022-05-19 10:36:42 +0200

    Efecte de morir particules

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -1,16 +1,17 @@
 extends Camera
 
-export var distance = 4.0
-export var height = 2.0
+export var distance = 18.0
+export var height = 9.0
 
 func _ready():
 	set_physics_process(true)
 	set_as_toplevel(true)
+	set_camera()
 
 func _physics_process(delta):
 	
-	var target_pos = get_parent().get_transform().origin
-	var pos = get_transform().origin
+	var target_pos = get_parent().get_global_transform().origin
+	var pos = get_global_transform().origin
 	var up = Vector3(0,1,0)
 	
 	var offset = pos - target_pos
@@ -23,3 +24,16 @@ func _physics_process(delta):
 	look_at_from_position(pos, target_pos, up)
 	 
 
+func set_camera():
+	
+	var target_pos = get_parent().get_global_transform().origin
+	var pos = get_global_transform().origin
+	
+	var offset = pos - target_pos
+	
+	offset = offset.normalized() * distance
+	offset.y = height
+	
+	pos = target_pos + offset
+	
+	#look_at_from_position(pos, target_pos, up)
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,7 +1,10 @@
 extends PathFollow
 
+const DeathEffect = preload("res://prefabs/DeathEffect.tscn")
+
 export(float) var velocity = 0.0
 var current_state = RUN
+var shoted = false
 
 onready var animationPlayer = $Player/AnimationPlayer
 
@@ -13,8 +16,17 @@ func _physics_process(delta):
 		offset = offset + velocity
 		if current_state == RUN:
 			animationPlayer.play("Run")
-		if offset >= 20:
-			queue_free()
+		if offset >= 5 and !shoted:
+			$Player.visible = false
+			var deathEffect = DeathEffect.instance()
+			add_child(deathEffect)
+			deathEffect.scale = Vector3(0.6,0.6,0.6)
+			deathEffect.global_transform = $DeathEffectSpawnPoint.global_transform
+			shoted = true
+			offset = 5
 	else:
 		current_state = IDLE
 		animationPlayer.play("Idle")
+
+func make_invisible():
+	$Player.visible = false
