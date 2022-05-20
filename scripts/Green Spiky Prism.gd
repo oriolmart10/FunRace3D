531a22d7df1c6964d011c0a798a2b107d2627d61
Adam-FIB-UPC <adam.lazaro@estudiantat.upc.edu>
2022-05-20 13:38:26 +0200

    Afegits Xurros4, Prisma Verd i Molí

---
+++ scripts/Green Spiky Prism.gd
@@ -0,0 +1,66 @@
+extends Spatial
+
+enum STATE {MOVING,STOPPED}
+
+enum DIRECTION {X_POS,X_NEG,Z_POS,Z_NEG}
+
+export (DIRECTION) var dir_chosen
+
+export var distance = 1
+
+export var velocity = 1.5
+
+var ini_time = 0
+var fin_time = 0
+
+var curr_state = STATE.MOVING
+
+var tot_trans = 0
+
+func _physics_process(delta):
+	match curr_state:
+		STATE.STOPPED:
+			fin_time = OS.get_ticks_msec()
+			var elapsed = fin_time - ini_time
+			if (elapsed >= 1500): 
+				curr_state = STATE.MOVING
+				fin_time = 0
+				ini_time = 0
+		STATE.MOVING:
+			match dir_chosen:
+				DIRECTION.X_POS:
+					tot_trans += abs(velocity*delta)
+					translate(Vector3(velocity*delta,0,0))
+					if (tot_trans >= distance):
+						curr_state = STATE.STOPPED
+						ini_time = OS.get_ticks_msec()
+						fin_time = ini_time
+						tot_trans = 0
+						dir_chosen = DIRECTION.X_NEG
+				DIRECTION.X_NEG:
+					tot_trans += abs(-velocity*delta)
+					translate(Vector3(-velocity*delta,0,0))
+					if (tot_trans >= distance):
+						curr_state = STATE.STOPPED
+						ini_time = OS.get_ticks_msec()
+						fin_time = ini_time
+						tot_trans = 0
+						dir_chosen = DIRECTION.X_POS
+				DIRECTION.Z_POS:
+					tot_trans += abs(velocity*delta)
+					translate(Vector3(0,0,velocity*delta))
+					if (tot_trans >= distance):
+						curr_state = STATE.STOPPED
+						ini_time = OS.get_ticks_msec()
+						fin_time = ini_time
+						tot_trans = 0
+						dir_chosen = DIRECTION.Z_NEG
+				DIRECTION.Z_NEG:
+					tot_trans += abs(-velocity*delta)
+					translate(Vector3(0,0,-velocity*delta))
+					if (tot_trans >= distance):
+						curr_state = STATE.STOPPED
+						ini_time = OS.get_ticks_msec()
+						fin_time = ini_time
+						tot_trans = 0
+						dir_chosen = DIRECTION.Z_POS
---
+++ scripts/Windmill.gd
@@ -0,0 +1,80 @@
+extends Spatial
+
+enum WINDMILL_TYPE {CONSTANT,SUDDEN}
+
+enum DIRECTION {CLOCKWISE,ANTICLOCKWISE}
+
+enum FACE_DIRECTION {Xs,Zs}
+
+enum STATE {STOPPED,MOVING}
+
+export (WINDMILL_TYPE) var wind_type
+export (DIRECTION) var direction
+export (FACE_DIRECTION) var face_dir
+
+var ini_time = 0
+var fin_time = 0
+
+var curr_state = STATE.MOVING
+
+var total_degrees = 0
+
+func _physics_process(delta):
+	match wind_type:
+		WINDMILL_TYPE.CONSTANT:
+			match direction:
+				DIRECTION.CLOCKWISE:
+					#rotate 0.5 degrees in y axis for each frame, more or less
+					var rotation = 0.167 * PI * delta
+					match face_dir:
+						FACE_DIRECTION.Xs:
+							rotate_x(-rotation)
+						FACE_DIRECTION.Zs:
+							rotate_z(-rotation)
+				DIRECTION.ANTICLOCKWISE:
+					#rotate -0.5 degrees in y axis for each frame, more or less
+					var rotation = 0.167 * PI * delta
+					match face_dir:
+						FACE_DIRECTION.Xs:
+							rotate_x(rotation)
+						FACE_DIRECTION.Zs:
+							rotate_z(rotation)
+		WINDMILL_TYPE.SUDDEN:
+			match curr_state:
+				STATE.STOPPED:
+					fin_time = OS.get_ticks_msec()
+					var elapsed = fin_time - ini_time
+					if (elapsed >= 1500): 
+						curr_state = STATE.MOVING
+						fin_time = 0
+						ini_time = 0
+				STATE.MOVING:
+					match direction:
+						DIRECTION.CLOCKWISE:
+							#rotate 1 degrees in y axis for each frame, more or less, until 90 are reached
+							var rotation = 0.25 * PI * delta
+							match face_dir:
+								FACE_DIRECTION.Xs:
+									rotate_x(-rotation)
+								FACE_DIRECTION.Zs:
+									rotate_z(-rotation)
+							total_degrees += rotation
+							if (total_degrees >= PI/2):
+								curr_state = STATE.STOPPED
+								ini_time = OS.get_ticks_msec()
+								fin_time = ini_time
+								total_degrees = 0
+						DIRECTION.ANTICLOCKWISE:
+							#rotate 1 degrees in y axis for each frame, more or less, until 90 are reached
+							var rotation = 0.25 * PI * delta
+							match face_dir:
+								FACE_DIRECTION.Xs:
+									rotate_x(rotation)
+								FACE_DIRECTION.Zs:
+									rotate_z(rotation)
+							total_degrees += rotation
+							if (total_degrees >= PI/2):
+								curr_state = STATE.STOPPED
+								ini_time = OS.get_ticks_msec()
+								fin_time = ini_time
+								total_degrees = 0
