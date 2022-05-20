e103887767b32ee3c51acc062412c2faa2925099
Oriol Martín <oriol.martin.olive@estudiantat.upc.edu>
2022-05-20 21:31:42 +0200

    Squashed commit of the following:
    commit 3d1c336bb7d83a26fcf5a685e0ba42e0b8b5273a
    Author: Oriol Martín <oriol.martin.olive@estudiantat.upc.edu>
    Date:   Fri May 20 21:05:02 2022 +0200
    
        Imports i demes
    
    commit c9baca457b78c014eaa15a24b9f595d986c09a72
    Merge: c246d3d 531a22d
    Author: Oriol Martín <oriol.martin.olive@estudiantat.upc.edu>
    Date:   Fri May 20 20:59:10 2022 +0200
    
        Merge remote-tracking branch 'origin/adam' into Backup1
    
    commit 531a22d7df1c6964d011c0a798a2b107d2627d61
    Author: Adam-FIB-UPC <adam.lazaro@estudiantat.upc.edu>
    Date:   Fri May 20 13:38:26 2022 +0200
    
        Afegits Xurros4, Prisma Verd i Molí
    
    commit fb06a92858faaa4d3caedcbd24c7eb0812d2c5ac
    Author: Adam-FIB-UPC <adam.lazaro@estudiantat.upc.edu>
    Date:   Tue May 10 11:44:37 2022 +0200
    
        Xurros (4) afegits
    
    commit 0e9d089778c0f3f1b031497de68ee4710c971069
    Author: Adam-FIB-UPC <adam.lazaro@estudiantat.upc.edu>
    Date:   Tue May 10 10:37:53 2022 +0200
    
        Branca actualitzada

---
+++ scripts/Xurros.gd
@@ -0,0 +1,61 @@
+extends Spatial
+
+enum XURRO_TYPE {CONSTANT,SUDDEN}
+
+enum DIRECTION {CLOCKWISE,ANTICLOCKWISE}
+
+enum STATE {STOPPED,MOVING}
+
+export (XURRO_TYPE) var xurro_type
+export (DIRECTION) var direction
+
+var ini_time = 0
+var fin_time = 0
+
+var curr_state = STATE.MOVING
+
+var total_degrees = 0
+
+func _physics_process(delta):
+	match xurro_type:
+		XURRO_TYPE.CONSTANT:
+			match direction:
+				DIRECTION.CLOCKWISE:
+					#rotate 0.5 degrees in y axis for each frame, more or less
+					var rotation = 0.167 * PI * delta
+					rotate_y(-rotation)
+				DIRECTION.ANTICLOCKWISE:
+					#rotate -0.5 degrees in y axis for each frame, more or less
+					var rotation = 0.167 * PI * delta
+					rotate_y(rotation)
+		XURRO_TYPE.SUDDEN:
+			match curr_state:
+				STATE.STOPPED:
+					fin_time = OS.get_ticks_msec()
+					var elapsed = fin_time - ini_time
+					if (elapsed >= 1500): 
+						curr_state = STATE.MOVING
+						fin_time = 0
+						ini_time = 0
+				STATE.MOVING:
+					match direction:
+						DIRECTION.CLOCKWISE:
+							#rotate 1 degrees in y axis for each frame, more or less, until 90 are reached
+							var rotation = 0.25 * PI * delta
+							rotate_y(-rotation)
+							total_degrees += rotation
+							if (total_degrees >= PI/2):
+								curr_state = STATE.STOPPED
+								ini_time = OS.get_ticks_msec()
+								fin_time = ini_time
+								total_degrees = 0
+						DIRECTION.ANTICLOCKWISE:
+							#rotate 1 degrees in y axis for each frame, more or less, until 90 are reached
+							var rotation = 0.25 * PI * delta
+							rotate_y(rotation)
+							total_degrees += rotation
+							if (total_degrees >= PI/2):
+								curr_state = STATE.STOPPED
+								ini_time = OS.get_ticks_msec()
+								fin_time = ini_time
+								total_degrees = 0
