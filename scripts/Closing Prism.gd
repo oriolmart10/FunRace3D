dff4975b01ff00b2a42f6f8f58f3c83e097d3613
Lanisos <83602198+Lanisos@users.noreply.github.com>
2022-05-22 23:34:06 +0200

    Algunes morts per obstacle fetes i intent d'arreglar prismes blaus

--- scripts/Closing Prism.gd
+++ scripts/Closing Prism.gd
@@ -4,7 +4,11 @@ enum STATE {MOVING,STOPPED}
 
 enum DIRECTION {X_POS,X_NEG,Z_POS,Z_NEG}
 
+enum TYPE {CLOSING,DISTANCE}
+
 export (DIRECTION) var dir_chosen
+
+export (TYPE) var prism_type
 
 export var distance = 1
 
@@ -16,6 +20,8 @@ var fin_time = 0
 var curr_state = STATE.MOVING
 
 var tot_trans = 0
+
+var counter_dir = false
 
 func _physics_process(delta):
 	match curr_state:
@@ -31,7 +37,7 @@ func _physics_process(delta):
 				DIRECTION.X_POS:
 					tot_trans += abs(velocity*delta)
 					translate(Vector3(velocity*delta,0,0))
-					if (tot_trans >= distance):
+					if (tot_trans >= distance and prism_type == TYPE.DISTANCE):
 						curr_state = STATE.STOPPED
 						ini_time = OS.get_ticks_msec()
 						fin_time = ini_time
@@ -40,7 +46,7 @@ func _physics_process(delta):
 				DIRECTION.X_NEG:
 					tot_trans += abs(-velocity*delta)
 					translate(Vector3(-velocity*delta,0,0))
-					if (tot_trans >= distance):
+					if (tot_trans >= distance and prism_type == TYPE.DISTANCE):
 						curr_state = STATE.STOPPED
 						ini_time = OS.get_ticks_msec()
 						fin_time = ini_time
@@ -49,7 +55,7 @@ func _physics_process(delta):
 				DIRECTION.Z_POS:
 					tot_trans += abs(velocity*delta)
 					translate(Vector3(0,0,velocity*delta))
-					if (tot_trans >= distance):
+					if (tot_trans >= distance and prism_type == TYPE.DISTANCE):
 						curr_state = STATE.STOPPED
 						ini_time = OS.get_ticks_msec()
 						fin_time = ini_time
@@ -58,7 +64,7 @@ func _physics_process(delta):
 				DIRECTION.Z_NEG:
 					tot_trans += abs(-velocity*delta)
 					translate(Vector3(0,0,-velocity*delta))
-					if (tot_trans >= distance):
+					if (tot_trans >= distance and prism_type == TYPE.DISTANCE):
 						curr_state = STATE.STOPPED
 						ini_time = OS.get_ticks_msec()
 						fin_time = ini_time
@@ -67,5 +73,22 @@ func _physics_process(delta):
 
 
 func _on_Area_area_entered(area):
-	
-	pass # Replace with function body.
+	curr_state = STATE.STOPPED
+	ini_time = OS.get_ticks_msec()
+	fin_time = ini_time
+	tot_trans = 0
+	match dir_chosen:
+		DIRECTION.X_POS:
+			dir_chosen = DIRECTION.X_NEG
+		DIRECTION.X_NEG:
+			dir_chosen = DIRECTION.X_POS
+		DIRECTION.Z_POS:
+			dir_chosen = DIRECTION.Z_NEG
+		DIRECTION.Z_NEG:
+			dir_chosen = DIRECTION.Z_POS
+			
+
+
+
+func _on_Area_body_entered(body):
+	body.death()
