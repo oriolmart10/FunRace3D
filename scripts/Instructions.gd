8fb30c6cff9505859d0c35d6067a7d08ec13bb58
Adam-FIB-UPC <adam.lazaro@estudiantat.upc.edu>
2022-05-24 13:33:48 +0200

    Closing Prisms fets, pantalla Instruccions bastant avançada

---
+++ scripts/Instructions.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+
+func _on_BackButton_pressed():
+	get_tree().change_scene("res://scenes/MainMenu.tscn")
--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://scenes/LevelCurve.tscn")
+
+
+func _on_InstrButton_pressed():
+	get_tree().change_scene("res://scenes/Instructions.tscn")
