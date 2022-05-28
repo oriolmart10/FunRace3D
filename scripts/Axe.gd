e917eb4f2aa10e0a2936b11519a0d38ddaa45f26
Oriol Martín <oriol.martin.olive@estudiantat.upc.edu>
2022-05-28 23:31:02 +0200

    Modif axe

--- scripts/Axe.gd
+++ scripts/Axe.gd
@@ -2,16 +2,5 @@ extends Spatial
 
 export(int) var DELAY = 0
 
-func _ready():
-	var t = Timer.new()
-	t.set_wait_time(DELAY)
-	t.set_one_shot(true)
-	self.add_child(t)
-	t.start()
-	yield(t, "timeout")
-	print("Play")
-	$AnimationPlayer.play("Swing")
-
-
 func _on_Area_body_entered(body):
 	body.death()
