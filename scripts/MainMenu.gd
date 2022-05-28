d156df9df2dc1678dd6e81568e228ce9d5c2e6e7
Lanisos <83602198+Lanisos@users.noreply.github.com>
2022-05-28 20:11:51 +0200

    Pantalla Credits, Exit i punxes gairebé implementades

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -3,6 +3,11 @@ extends Control
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://scenes/LevelCurve.tscn")
 
-
 func _on_InstrButton_pressed():
 	get_tree().change_scene("res://scenes/Instructions.tscn")
+
+func _on_CreditsButton_pressed():
+	get_tree().change_scene("res://scenes/Credits.tscn")
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
--- scripts/Spike Block.gd
+++ scripts/Spike Block.gd
@@ -22,12 +22,14 @@ var Spikes = preload("res://prefabs/Spikes.tscn")
 func activ_timer():
 	fin_time = OS.get_ticks_msec()
 	var elapsed = fin_time - ini_time
-	if (elapsed >= 1500): 
+	if (elapsed >= 1000): 
 		curr_state = STATE.WARNING
-		fin_time = 0
-		ini_time = 0
-		var mat = ResourceLoader.load("res://models/spike_block_release.png")
-		$MeshInstance.set_surface_material(0,mat)
+		ini_time = OS.get_ticks_msec()
+		fin_time = ini_time
+		var mat = SpatialMaterial.new()
+		var tex = preload("res://models/spike_block_release.png")
+		mat.albedo_texture = tex
+		$MeshInstance.material_override = mat
 		
 func warn_timer():
 	fin_time = OS.get_ticks_msec()
@@ -36,17 +38,21 @@ func warn_timer():
 		curr_state = STATE.RELEASING
 		fin_time = 0
 		ini_time = 0
-		var mat = ResourceLoader.load("res://models/spike_block.png")
-		$MeshInstance.set_surface_material(0,mat)
+		var mat = SpatialMaterial.new()
+		var tex = preload("res://models/spike_block.png")
+		mat.albedo_texture = tex
+		$MeshInstance.material_override = mat
 		var spikes = Spikes.instance()
 		child = spikes
 		add_child(spikes)
 		
 func raise_timer():
-	child.translate(Vector3(0.0, 0.5, 0.0))
-	tot_trans += 0.5
-	if (tot_trans >= 3):
+	child.translate(Vector3(0.0, 0.05, 0.0))
+	tot_trans += 0.05
+	if (tot_trans >= 1.7):
 		curr_state = STATE.RELEASED
+		ini_time = OS.get_ticks_msec()
+		fin_time = ini_time
 		
 func released_timer():
 	fin_time = OS.get_ticks_msec()
@@ -57,8 +63,8 @@ func released_timer():
 		ini_time = 0
 		
 func retract_timer():
-	child.translate(Vector3(0.0, -0.5, 0.0))
-	tot_trans -= 0.5
+	child.translate(Vector3(0.0, -0.05, 0.0))
+	tot_trans -= 0.05
 	if (tot_trans <= 0):
 		curr_state = STATE.INACTIVE
 		already_active = false
@@ -71,14 +77,19 @@ func _physics_process(delta):
 			pass
 		STATE.ACTIVATED:
 			activ_timer()
+			print("activated")
 		STATE.WARNING:
 			warn_timer()
+			print("warn")
 		STATE.RELEASING:
 			raise_timer()
+			print("reling")
 		STATE.RELEASED:
 			released_timer()
+			print("rel")
 		STATE.RETRACT:
 			retract_timer()
+			print("retr")
 
 func _on_Area_body_entered(body):
 	if (not already_active):
