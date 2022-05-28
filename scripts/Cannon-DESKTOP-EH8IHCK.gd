c30dfbfc2a0f2fdc84ea078db99adee5178e871d
Oriol Martín <oriol.martin.olive@estudiantat.upc.edu>
2022-05-28 23:24:24 +0200

    Borrats arxius duplicats

--- scripts/Cannon-DESKTOP-EH8IHCK.gd
+++
@@ -1,9 +0,0 @@
-extends Spatial
-
-var Bullet = preload("res://prefabs/Bullet.tscn")
-
-func _on_Timer_timeout():
-	var bullet = Bullet.instance()
-	add_child(bullet)
-	bullet.translate(Vector3(0.0, 3.4, 0.0))
-
--- scripts/MainMenu-DESKTOP-EH8IHCK.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _on_PlayButton_pressed():
-	get_tree().change_scene("res://scenes/LevelCurve.tscn")
-
-
-func _on_InstrButton_pressed():
-	get_tree().change_scene("res://scenes/Instructions.tscn")
